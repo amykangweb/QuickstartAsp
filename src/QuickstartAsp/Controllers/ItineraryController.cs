0bf3965385cba9f6d892fee1dbb0558c66e5c79a
amykangweb <selene6023@gmail.com>
2016-05-06 07:10:37 -0700

    Add itineraryitem form to itinerary index view. And create action to itinerary controller.

--- src/QuickstartAsp/Controllers/ItineraryController.cs
+++ src/QuickstartAsp/Controllers/ItineraryController.cs
@@ -16,5 +16,20 @@ namespace QuickstartAsp.Controllers
             var model = new ItineraryItem();
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(ItineraryItem data)
+        {
+            if (ModelState.IsValid)
+            {
+                // Handle saving the data only if
+                // the model state is valid
+            }
+            else
+            {
+                ModelState.AddModelError("", "The data you entered was not valid");
+            }
+            return View();
+        }
     }
 }
