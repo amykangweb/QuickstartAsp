a2677995c7728a6f00aa4321fd6e779a39ca15ce
amykangweb <selene6023@gmail.com>
2016-05-09 17:45:08 -0700

    Setup DB context, create post model and post controller. Only allow logged in users to access create, edit, delete actions.

---
+++ QuickStartAsp/DAL/QuickContext.cs
@@ -0,0 +1,18 @@
+﻿using QuickStartAsp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace QuickStartAsp.DAL
+{
+    public class QuickContext : DbContext
+    {
+        public QuickContext() : base("QuickContext") { }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
